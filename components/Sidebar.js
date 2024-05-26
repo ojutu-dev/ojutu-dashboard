62f53735a9981cb7f593670112996a5073401384
Afolabi <linktoyinka@gmail.com>
2024-05-26 14:22:51 +0100

    chore: Remove commented out code and import next/navigation in Sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,38 +1,3 @@
-// import { useRouter } from 'next/navigation';
-
-// const menuItems = [
-//   'Post',
-//   'Portfolio',
-//   'Testimonials',
-//   'Brands',
-//   'Authors',
-//   'Categories',
-//   'Keywords',
-//   'Service',
-// ];
-
-// export default function Sidebar() {
-//   const router = useRouter();
-
-//   return (
-//     <div className="w-1/5 h-screen p-4 bg-gray-800 text-white sticky top-0">
-//       <ul>
-//         {menuItems.map((item) => (
-//           <li 
-//             key={item} 
-//             className="mb-4 cursor-pointer hover:text-gray-300"
-//             onClick={() => router.push(`/dashboard/${item.toLowerCase()}`)}
-//           >
-//             {item}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-
-
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
