fe121b83e902865ad72a7a29ba88f6f362882201
gbengaO <gbenga@ojutu.co>
2024-05-27 13:21:45 +0200

    other endpoint

---
+++ pages/api/brand/index.js
@@ -0,0 +1,27 @@
+import connectToMongoDB from '../../../libs/mongodb';
+import Brand from '../../../model/brand';
+
+export default async function handler(req, res) {
+  await connectToMongoDB(process.env.MONGODB_URI);
+
+  if (req.method === 'GET') {
+    try {
+      const brand = await Brand.find();
+      res.status(200).json({success:true, data:brand});
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching brand', error });
+      console.log("Error Messgage:", error)
+    }
+  } else if (req.method === 'POST') {
+    try {
+      const { title, description } = req.body;
+      const newBrand = new Brand({ title, description });
+      await newBrand.save();
+      res.status(201).json(newBrand);
+    } catch (error) {
+      res.status(500).json({ message: 'Error creating category', error });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
