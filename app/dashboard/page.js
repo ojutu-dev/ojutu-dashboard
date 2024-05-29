67877bb6f9d59ab539f82582c87fca226c0e2626
Afolabi <linktoyinka@gmail.com>
2024-05-29 01:20:34 +0100

    refactor: Update Dashboard page to display a welcome message

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -13,8 +13,8 @@ export default function Dashboard() {
   return (
     <div className="flex">
       <div className="flex flex-col w-full">
-        <div className="p-4">
-          <h1>Dashboard</h1>
+        <div className="p-4 grid place-content-center w-full h-[50vh]">
+          <h1>Welcome Abroad</h1>
         </div>
       </div>
     </div>
