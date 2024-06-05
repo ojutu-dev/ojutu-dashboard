c643b258d018c3986e7fff50782f752d3142268e
gbengaO <gbenga@ojutu.co>
2024-06-05 13:24:03 +0200

    cloudflare deployment

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientProviders from '../components/ClientProviders';
+export const runtime = "edge"
 
 const inter = Inter({ subsets: ["latin"] });
 
