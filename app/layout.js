64cb8d3953078db13a3a7ad31113c279a795f836
gbengaO <gbenga@ojutu.co>
2024-06-12 00:09:41 +0200

    not found page

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientProviders from '../components/ClientProviders';
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
